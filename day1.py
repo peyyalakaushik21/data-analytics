ac449bd645d2bdafe858a16918c430b03e33396a
peyyalakaushik21 <peyyalakaushik21@gmail.com>
2026-05-18 16:07:00 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,44 @@
+#print("hello world")
+'''
+what is python
+-high level language
+------no need to worry about low level management.
+dynamically typed language
+-----no need to mention the kind of data taking to variable.
+'''
+num = 23
+so = "python"
+any = [1,2]
+an = {"name":"karthi"}
+print(num)
+print(so)
+print(any)
+print(an)
+'''
+interperter language
+----it executes the code line by line and can through the error if any but all the before lines will execute and gives output
+
+Tokens
+
+--Tokens are the key words in the program.
+ex: if,else,while,for
+
+Identifiers:
+---identifiers are the name of variables,function and classes.
+
+Variables:
+----variables are started with capital or small alphabets.
+Ex: good ways
+----A-Z,a-z,_
+-----bad ways
+  special char,space ,0-9
+
+Comments
+----1. single line (#)
+----2. multiline('''''')
+
+Boolean(true or false):
+'''
+a=9
+b=7
+print(a==b)
