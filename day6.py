28c77fd3c882f485cfb02a7b2bcda9a54833ba4b
peyyalakaushik21 <peyyalakaushik21@gmail.com>
2026-05-19 15:54:40 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,91 @@
+'''
+syntax-->variable_name.discard(element)
+
+type conversions
+----------------
+
+int-->
+
+
+an = 78
+us = str(an)
+om = float(an)
+print(om)
+print(type(om))
+print(type(us))
+
+str-->
+
+
+an = "90"
+Ear = list(an)
+print(Ear)
+print(type(Ear))
+con = tuple(an)
+print(con)
+
+float-->
+
+
+Car = 90.78
+print(int(Car))
+print(type(str(Car)))
+
+list-->
+
+
+Any = [6,7]
+print(str(Any))
+print(tuple(Any))
+
+
+Tuple-->
+
+
+how = (4,5)
+print(list(how))
+print(str(how))
+
+
+int as a user-input
+-------------------
+num = int(input("Enter a number: "))
+print(89+num)
+
+str as a user-input
+-------------------
+some = input("Write a text: ")
+
+
+any = input("Enter number: ").split()
+print(any)
+
+
+list as a user-input
+--------------------
+any = list(map(int,input("Enter numbers: ").split()))
+print(any)
+
+
+
+Tuple as a user-input
+---------------------
+an = tuple(map(int,input("Enter numbers: ").split()))
+print(an)
+
+
+'''
+
+num  = eval(input("Enter: "))
+print(type(num))
+
+
+
+
+
+
+
+
+
+
+
